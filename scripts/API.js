f0924b43f564d34229e4cf932a2312faa0eb5789
Kristen Bartel <kristenbartel@gmail.com>
2022-01-18 18:08:03 -0600

    API

--- scripts/API.js
+++ scripts/API.js
@@ -1,2 +1,7 @@
-let mapsKey = `AIzaSyCpxk5yKoAcrMtrTsda23tLvqIZ4RAJCZk`;
+
+module.exports = {    
+    "goodKey": "your developer key",    
+    "goodSecret": "secrete",    
+    "goodUserID": 12345
+  }let mapsKey = `AIzaSyCpxk5yKoAcrMtrTsda23tLvqIZ4RAJCZk`;
 let trailsKey = `BFF8C027-7C8F-480B-A5F8-CD8CE490BFBA`;
