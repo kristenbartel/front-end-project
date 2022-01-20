01f1e189e08032228c456865824b8ede63dd289f
eugenejac3 <eugene.jackson9@gmail.com>
2022-01-19 19:56:24 -0600

    added event listener, working on using multiple inputs

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -6,10 +6,49 @@ let logMapData = (mapData) => {
     console.log(mapData);
     return mapData;
 }
-// ---------------fetch----------------
-function getMapData() {
-    console.log(keys.keys.mapsKey)
-    const latLng = fetch (`https://maps.googleapis.com/maps/api/geocode/json?address=houston&key=${keys.keys.mapsKey}`)
+// ---------------fetch---------------- *Eugene* 1.19.21 Commented test fetch to ensure event listener was working
+
+// function getMapData() {
+//     console.log(keys.keys.mapsKey)
+//     const latLng = fetch (`https://maps.googleapis.com/maps/api/geocode/json?address=houston&key=${keys.keys.mapsKey}`)
+//     .then(response => response.json())
+//     .then(latData => {
+//         console.log(latData);
+//         let lat = latData.results[0].geometry.location.lat;
+//         let lng = latData.results[0].geometry.location.lng;
+//         console.log(lat);
+//         console.log(lng);
+//         return fetch(`https://trailapi-trailapi.p.rapidapi.com/trails/explore/?lat=${lat}&lon=${lng}`, {
+//             "method": "GET",
+//             "headers": {
+//                 "x-rapidapi-host": "trailapi-trailapi.p.rapidapi.com",
+//                 "x-rapidapi-key": `${keys.keys.trailsKey}`
+//             }
+//         })
+//         .then(response => response.json())
+//         .then(data => {
+//             console.log(data)
+//         })
+//     })
+// }
+// getMapData();
+
+// ---------DOM declarations and bindings----------------
+
+const header = document.getElementById('header');
+let userInput = document.getElementById('userInput');
+let inputText = userInput.value
+let searchButton = document.getElementById('searchButton');
+let searchResults = document.getElementById('searchResults');
+let footer = document.getElementById('footer');
+
+
+
+// Event listner for 'searchButton'
+
+searchButton.addEventListener('click', (e) =>{
+    e.preventDefault();
+    fetch (`https://maps.googleapis.com/maps/api/geocode/json?address=${inputText}&key=${keys.keys.mapsKey}`)
     .then(response => response.json())
     .then(latData => {
         console.log(latData);
@@ -27,15 +66,10 @@ function getMapData() {
         .then(response => response.json())
         .then(data => {
             console.log(data)
+            
+            inputText = "";
         })
     })
-}
-getMapData();
+    
+});
 
-// ---------DOM declarations and bindings----------------
-
-let header = document.getElementsById('header');
-let userInput = document.getElementsById('userInput');
-let searchButton = document.getElementById('searchButton');
-let searchResults = document.getElementById('searchResults');
-let footer = document.getElementsById('footer');
